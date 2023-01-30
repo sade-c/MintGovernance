dc1fdba65d1ee63a124d8ef8de1f14b3513142c0
zinderud <mokosam@gmail.com>
2023-01-30 19:04:48 +0300

    one

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_ALCHEMY_RPC_URL = process.env.GOERLI_ALCHEMY_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0".repeat(64);
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: GOERLI_ALCHEMY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+    },
+  },
 };
--- test/MyGovernor.js
+++ test/MyGovernor.js
@@ -2,21 +2,22 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { assert } = require("chai");
 const { ethers } = require("hardhat");
 const { toUtf8Bytes, keccak256, parseEther } = ethers.utils;
+const moveBlocks = require("../utils/moveBlocks");
 
 describe("MyGovernor", function () {
   async function deployFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const transactionCount = await owner.getTransactionCount();
-  
+
     // gets the address of the token before it is deployed
     const futureAddress = ethers.utils.getContractAddress({
       from: owner.address,
-      nonce: transactionCount + 1
+      nonce: transactionCount + 1,
     });
 
     const MyGovernor = await ethers.getContractFactory("MyGovernor");
-    const governor = await MyGovernor.deploy(futureAddress);
+    const governor = await MyGovernor.deploy(futureAddress, 10, 20);
 
     const MyToken = await ethers.getContractFactory("MyToken");
     const token = await MyToken.deploy(governor.address);
@@ -41,55 +42,88 @@ describe("MyGovernor", function () {
       const tx = await governor.propose(
         [token.address],
         [0],
-        [token.interface.encodeFunctionData("mint", [owner.address, parseEther("25000")])],
+        [
+          token.interface.encodeFunctionData("mint", [
+            owner.address,
+            parseEther("25000"),
+          ]),
+        ],
         "Give the owner more tokens!"
       );
       const receipt = await tx.wait();
-      const event = receipt.events.find(x => x.event === 'ProposalCreated');
+      const event = receipt.events.find((x) => x.event === "ProposalCreated");
       const { proposalId } = event.args;
 
-      // wait for the 1 block voting delay
-      await hre.network.provider.send("evm_mine");
-      
-      return { ...deployValues, proposalId } 
+      return { ...deployValues, proposalId };
     }
-    
+
     it("should set the initial state of the proposal", async () => {
       const { governor, proposalId } = await loadFixture(afterProposingFixture);
-      
+
       const state = await governor.state(proposalId);
       assert.equal(state, 0);
     });
-    
+
     describe("after voting", () => {
       async function afterVotingFixture() {
         const proposingValues = await afterProposingFixture();
         const { governor, proposalId } = proposingValues;
-        
-        const tx = await governor.castVote(proposalId, 1);      
-        const receipt = await tx.wait();
-        const voteCastEvent = receipt.events.find(x => x.event === 'VoteCast');
-        
-        // wait for the 1 block voting period
-        await hre.network.provider.send("evm_mine");
 
-        return { ...proposingValues, voteCastEvent }
+        let proposalState = await governor.state(proposalId);
+        console.log(`Current Proposal State: ${proposalState}`);
+
+        // wait for the votingDelay blocks voting delay
+        let votingDelay = await governor.votingDelay();
+        await moveBlocks(votingDelay.toNumber() + 1);
+
+        const tx = await governor.castVote(proposalId, 1);
+        const receipt = await tx.wait();
+        const voteCastEvent = receipt.events.find(
+          (x) => x.event === "VoteCast"
+        );
+
+        proposalState = await governor.state(proposalId);
+        assert.equal(proposalState.toString(), "1");
+        console.log(`Current Proposal State: ${proposalState}`);
+
+        // wait for the votingDelay blocks voting delay
+        votingDelay = await governor.votingDelay();
+        await moveBlocks(votingDelay.toNumber() + 1);
+
+        return { ...proposingValues, voteCastEvent };
       }
 
       it("should have set the vote", async () => {
         const { voteCastEvent, owner } = await loadFixture(afterVotingFixture);
 
         assert.equal(voteCastEvent.args.voter, owner.address);
-        assert.equal(voteCastEvent.args.weight.toString(), parseEther("10000").toString());
+        assert.equal(
+          voteCastEvent.args.weight.toString(),
+          parseEther("10000").toString()
+        );
       });
 
       it("should allow executing the proposal", async () => {
-        const { governor, token, owner } = await loadFixture(afterVotingFixture);
+        const { governor, proposalId, token, owner } = await loadFixture(
+          afterVotingFixture
+        );
+
+        // wait for the votingPeriod to pass...
+        const votingPeriod = await governor.votingPeriod();
+        await moveBlocks(votingPeriod.toNumber() + 1);
+
+        proposalState = await governor.state(proposalId);
+        console.log(`Current Proposal State: ${proposalState}`);
 
         await governor.execute(
           [token.address],
           [0],
-          [token.interface.encodeFunctionData("mint", [owner.address, parseEther("25000")])],
+          [
+            token.interface.encodeFunctionData("mint", [
+              owner.address,
+              parseEther("25000"),
+            ]),
+          ],
           keccak256(toUtf8Bytes("Give the owner more tokens!"))
         );
 
